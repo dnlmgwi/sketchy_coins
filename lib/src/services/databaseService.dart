e8e02471f5470a3dbbc38c2510b4ee1fd6f2f8a5
Daniel P. Mgawi <54575392+sketchdm@users.noreply.github.com>
2021-05-03 15:16:39 +0200

    Update databaseService.dart
    - Env Changes

--- lib/src/services/databaseService.dart
+++ lib/src/services/databaseService.dart
@@ -1,9 +1,15 @@
 import 'package:sketchy_coins/packages.dart';
 
 class DatabaseService {
-  static final supabaseUrl = 'https://shepbpgqtqyeddohurey.supabase.co';
-  static final supabaseKey =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYxOTkxMjAwNCwiZXhwIjoxOTM1NDg4MDA0fQ.6RRIJNmdBJ6atvWcJmnLrgCME2E53KZNiE4eC3j0BKA';
+  static final envSupabaseUrl = Platform.environment['SUPABASE_URL'];
+  static const localSupabaseUrl = 'https://yhidmwnuvklxbiqbhzpv.supabase.co';
+  static String supabaseUrl = envSupabaseUrl ?? localSupabaseUrl;
+
+  static final envSupabaseKey = Platform.environment['SUPABASE_KEY'];
+  static const localSupabaseKey =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyMDA0NzE0MCwiZXhwIjoxOTM1NjIzMTQwfQ.07I6oe0MGd66Wy8wnqh4pd2Ya3qjyt_qT2l4K1ZFb-4';
+  static String supabaseKey = envSupabaseKey ?? localSupabaseKey;
+
   final client = PostgrestClient('$supabaseUrl/rest/v1',
       headers: {'apikey': supabaseKey}, schema: 'public');
 }
